280631eaad0a1d95cf1be096d22ab19bf6010bf4
emiliayoffie <emiliayoffie@gmail.com>
2024-01-22 11:04:26 -0500

    adds header, footer, welcome screen

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      Built by Emilia Yoffie
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>BATTLESHIP</h1>
+    </header>
+  );
+};
+
+export default Header;
