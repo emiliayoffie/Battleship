d27baa856acf1a6c72e70e2d4d27c7cb2c6d9135
emiliayoffie <emiliayoffie@gmail.com>
2024-01-23 20:30:31 -0500

    updates styling to be more mobile friendly

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import WelcomeScreen from '../WelcomeScreen/WelcomeScreen';
 import { Game } from '../Game/Game';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
 
 import '../../css/style.css';
 
@@ -13,12 +11,10 @@ const App = () => {
     setAppState('play');
   };
 
-  // Renders either Welcome Screen or Game
+  /*Renders either Welcome Screen or Game*/
   return (
     <React.Fragment>
-      <Header />
       {appState === 'play' ? <Game /> : <WelcomeScreen startPlay={startPlay} />}
-      <Footer />
     </React.Fragment>
   );
 };
